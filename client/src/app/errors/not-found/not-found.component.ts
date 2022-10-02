0f06ecf8ab50df635c6af693b1a07b40a17dded8
wiktord2000 <wiktord2000@wp.pl>
2022-10-02 13:48:53 +0200

    Add Not Found Page

---
+++ client/src/app/errors/not-found/not-found.component.ts
@@ -0,0 +1,16 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.css']
+})
+export class NotFoundComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+}
