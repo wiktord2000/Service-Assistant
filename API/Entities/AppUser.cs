cef5310852cc9442e2ba28b0eeb7a2c18c507cd1
wiktord2000 <wiktord2000@wp.pl>
2022-10-04 23:15:05 +0200

    Update AppUser and add Orders

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -11,5 +11,21 @@ namespace API.Entities
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public string CompanyName { get; set; }
+        public int Nip { get; set; }
+        public string Voivodeship { get; set; }     //województwo
+        public string Township { get; set; }        // powiat
+        public string Borough { get; set; }         // gmina
+        public int PostalCode { get; set; }
+        public string Town { get; set; }
+        public string Country { get; set; }
+        public string Street { get; set; }
+        public string HouseNumber { get; set; }
+        public int ContactNumber { get; set; }
+        public string Email { get; set; }
+        public int AccountNumber { get; set; }
+        public string BankName { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
     }
 }
