bc0fb7edb909f4e2587dcf71c111ee9ebec39d7e
wiktord2000 <wiktord2000@wp.pl>
2022-10-16 23:06:11 +0200

    Create orders service

---
+++ client/src/app/_services/orders.service.ts
@@ -0,0 +1,25 @@
+import { Order } from './../_models/Order';
+import { map, Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrdersService {
+
+  private baseUrl: String = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getOrders(statusPostions?: number[]){
+
+    // If no array provided - simple request
+    if(!statusPostions) return this.http.get<Order[]>(this.baseUrl + "orders");
+
+    // Otherwise - append query params
+    let params = statusPostions.reduce((params, statusPosition) => params.append('statusPositions', statusPosition), new HttpParams());
+    return this.http.get<Order[]>(this.baseUrl + "orders", {params});
+  }
+}
