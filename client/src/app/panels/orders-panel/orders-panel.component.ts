16f19941129e92284ab2066eaa395c0ad816ad9c
wiktord2000 <wiktord2000@wp.pl>
2022-10-17 22:00:42 +0200

    Update orders panel

--- client/src/app/panels/orders-panel/orders-panel.component.ts
+++ client/src/app/panels/orders-panel/orders-panel.component.ts
@@ -1,30 +1,36 @@
-import { HttpClient } from '@angular/common/http';
-import { getLocaleFirstDayOfWeek } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
+// import orders from '../../_seed-data/oders.json' and in the class we have to add prop ->  orders: Order[] = orders;      // Seed data from json file
+import { OrdersTableComponent } from './orders-table/orders-table.component';
+import { OrdersService } from './../../_services/orders.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { SnackbarService } from 'src/app/_services/snackbar.service';
-import orders from '../../_seed-data/oders.json'
-import { Order } from 'src/app/_models/Order';
+import { FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-orders-panel',
   templateUrl: './orders-panel.component.html',
   styleUrls: ['./orders-panel.component.css']
 })
-export class OrdersPanelComponent implements OnInit {
+export class OrdersPanelComponent implements OnInit, AfterViewInit {
 
-  constructor(public snackbarService : SnackbarService, private http: HttpClient) {
+  @ViewChild(OrdersTableComponent) ordersTable!: OrdersTableComponent;
+  statusControl = new FormControl(['0','1','2','3']);
+  
+  constructor(public snackbarService : SnackbarService, 
+              public ordersService: OrdersService) {
+  }
+  ngAfterViewInit(): void {
+    this.ordersTable.dataSource.loadOrders(this.statusControl.value.map(Number));
   }
 
-  orders: Order[] = orders;
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onToggleGroupChange(){
+    (!this.statusControl.value.length) 
+      ? this.ordersTable.dataSource.setOrders([])
+      : this.ordersTable.dataSource.loadOrders(this.statusControl.value.map(Number));
   }
-
-  showSuccess(){
-    let ref = this.snackbarService.showMessage('error',  "To jest normalny", "OKO", false);
-  }
-
-  showWarn(){
-    let ref = this.snackbarService.showMessage('warn',  "To jest normalny", "OKO", false);
-  }
+  
 }
+
