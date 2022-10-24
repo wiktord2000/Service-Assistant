129f4d455b4cb0bcd6d2b01232686a2769454abb
wiktord2000 <wiktord2000@wp.pl>
2022-10-24 22:21:03 +0200

    Add new Dtos

---
+++ API/DTOs/ClientDto.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class ClientDto
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string CompanyName { get; set; }
+        public string Nip { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string CountryCode { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public int? DiscountJobs { get; set; }
+        public int? DiscountParts { get; set; }
+        public DateTime CreatedAt { get; set; } 
+        public DateTime UpdatedAt { get; set; } 
+        public ICollection<VehicleDto> Vehicles { get; set; }
+    }
+}
---
+++ API/DTOs/VehicleDto.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class VehicleDto
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string Vin { get; set; }
+        public string EngineCode { get; set; }
+        public int? Capacity { get; set; }
+        public string EngineFuel { get; set; }
+        public int? EnginePower { get; set; }
+        public string EnginePowerUnit { get; set; }
+        public string Color { get; set; }
+        public int? ProductionDate { get; set; }
+        public DateTime? FirstRegistration { get; set; }
+        public DateTime? TechnicalInspectionEnd { get; set; }
+        public string Description { get; set; }
+        public ClientDto CurrentOwner { get; set; }
+        public int CurrentOwnerId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
