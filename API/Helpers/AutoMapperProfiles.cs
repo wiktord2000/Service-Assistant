0963b4c0890dc5f9a80bfbc093c1130323029e8e
wiktord2000 <wiktord2000@wp.pl>
2022-10-31 23:10:03 +0100

    Update AutoMapperProfile

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -11,9 +11,14 @@ namespace API.Helpers
             // Map <From , To>
             CreateMap<Client, ClientDto>();
             CreateMap<Client, ClientDetailsDto>();
+            CreateMap<Client, ClientDetailsDto>().ReverseMap();
             CreateMap<Vehicle, VehicleDto>();
             CreateMap<Vehicle, VehicleDetailsDto>();
             CreateMap<Order, OrderDto>();
+            CreateMap<Order, OrderDto>().ReverseMap();
+            CreateMap<Status, StatusDto>();
+            CreateMap<Status, StatusDto>().ReverseMap();
+            CreateMap<ClientUpdateDto, Client>();
             // CreateMap<Order, OrderDto>();
         }
     }
