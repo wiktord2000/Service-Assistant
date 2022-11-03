242442dbf977dc74b175b08545b2cb9142fea3d5
wiktord2000 <wiktord2000@wp.pl>
2022-11-03 08:53:34 +0100

    Create client profile link

---
+++ client/src/app/_shared/client-profile-link/client-profile-link.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+import { TooltipPosition } from '@angular/material/tooltip';
+import { Client } from 'src/app/_models/Client';
+
+@Component({
+  selector: 'app-client-profile-link',
+  templateUrl: './client-profile-link.component.html',
+  styleUrls: ['./client-profile-link.component.css']
+})
+export class ClientProfileLinkComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() client: Client;
+  @Input() tooltipPosition: TooltipPosition = "right";
+  @Input() color: ThemePalette = "primary";
+  @Input() customColor?: string;
+  @Input() containLabel: boolean = false;
+  isCompany: boolean;
+
+  ngOnInit(): void {
+    this.isCompany = this.client.type === 'company';
+  }
+
+}
