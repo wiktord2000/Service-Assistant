b09d7de35c682fa05b02b51111b795cfcb947bde
wiktord2000 <wiktord2000@wp.pl>
2022-11-03 08:55:52 +0100

    Initial vehicle profile

--- client/src/app/profiles/vehicle-profile/vehicle-profile.component.ts
+++ client/src/app/profiles/vehicle-profile/vehicle-profile.component.ts
@@ -1,4 +1,6 @@
+import { SnackbarService } from 'src/app/_services/snackbar.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Vehicle } from '../../_models/Vehicle';
 import { VehiclesService } from '../../_services/vehicles.service';
@@ -11,19 +13,49 @@ import { VehiclesService } from '../../_services/vehicles.service';
 export class VehicleProfileComponent implements OnInit {
 
   vehicle: Vehicle; 
+  editForm : FormGroup;
+  isSaving: boolean = false;
 
   constructor(private vehiclesService: VehiclesService,
-              private activatedRoute: ActivatedRoute ) { }
+    private formBuilder: FormBuilder,
+    private snackbarService: SnackbarService,
+    private activatedRoute: ActivatedRoute ) { }
 
-  ngOnInit(): void {
-    this.loadVehicle();
-  }
+ngOnInit(): void {
+  this.loadVehicle();
+}
 
   loadVehicle(){
 
     const vehicleId = Number(this.activatedRoute.snapshot.paramMap.get('id'));
     this.vehiclesService.getVehicle(vehicleId).subscribe(vehicle => {
+      // Store client data
       this.vehicle = vehicle;
+
+      // Obtain owner name
+      const currentOwnerName = vehicle.currentOwner.type === 'company' 
+                                  ? vehicle.currentOwner.companyName 
+                                  : vehicle.currentOwner.firstname + " " + vehicle.currentOwner.lastname;
+
+      // Build form
+      this.editForm = this.formBuilder.group(
+        {
+          brand: [ vehicle.brand, [Validators.required]],
+          model: [ vehicle.model, [Validators.required]],
+          color: [ vehicle.color],
+          registrationNumber: [vehicle.registrationNumber],
+          productionDate: [vehicle.productionDate],
+          currentOwner: [currentOwnerName],
+          engineFuel: [vehicle.engineFuel],
+          vin: [vehicle.vin],
+          engineCode: [vehicle.engineCode],
+          capacity: [vehicle.capacity],
+          enginePower: [vehicle.enginePower],
+          technicalInspectionEnd: [vehicle.technicalInspectionEnd],
+          firstRegistration: [new Date(vehicle.firstRegistration).toLocaleDateString()],
+          description: [vehicle.description],
+        }
+      );
     });
   }
 
