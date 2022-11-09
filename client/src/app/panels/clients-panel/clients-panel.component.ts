dd3244105244a0a4334d52853193f739edf1e8c6
wiktord2000 <wiktord2000@wp.pl>
2022-11-10 00:46:21 +0100

    Update ClientsPanelComponent

--- client/src/app/panels/clients-panel/clients-panel.component.ts
+++ client/src/app/panels/clients-panel/clients-panel.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ClientsService } from 'src/app/_services/clients.service';
+import { SnackbarService } from 'src/app/_services/snackbar.service';
+import { ClientsTableComponent } from 'src/app/_shared/tables/clients-table/clients-table.component';
 
 @Component({
   selector: 'app-clients-panel',
@@ -7,7 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientsPanelComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(ClientsTableComponent) clientsTable!: ClientsTableComponent;
+  
+  constructor(public snackbarService : SnackbarService, 
+              public clientsService: ClientsService) {
+  }
+  ngAfterViewInit(): void {
+    this.clientsTable.dataSource.loadClients();
+  }
 
   ngOnInit(): void {
   }
