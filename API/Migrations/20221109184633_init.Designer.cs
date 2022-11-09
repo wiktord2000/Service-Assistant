8dc8ef05b49379ad8eb22696337923c09dd600fa
wiktord2000 <wiktord2000@wp.pl>
2022-11-09 20:39:13 +0100

    Update Migrations

--- API/Migrations/20221109184633_init.Designer.cs
+++ API/Migrations/20221109184633_init.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20221101192911_init")]
+    [Migration("20221109184633_init")]
     partial class init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
