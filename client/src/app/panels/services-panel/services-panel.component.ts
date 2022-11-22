dca5f376950e9def72dd320a5b3f25da01320edd
wiktord2000 <wiktord2000@wp.pl>
2022-11-22 22:41:44 +0100

    Update ServicesPanel

--- client/src/app/panels/services-panel/services-panel.component.ts
+++ client/src/app/panels/services-panel/services-panel.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Service } from 'src/app/_models/Service';
+import { ServicesService } from 'src/app/_services/services.service';
+import { SnackbarService } from 'src/app/_services/snackbar.service';
+import { CreateServiceDialogComponent } from 'src/app/_shared/_dialogs/create-service-dialog/create-service-dialog.component';
+import { ServicesTableComponent } from 'src/app/_shared/_tables/services-table/services-table.component';
 
 @Component({
   selector: 'app-services-panel',
   templateUrl: './services-panel.component.html',
   styleUrls: ['./services-panel.component.css']
 })
-export class ServicesPanelComponent implements OnInit {
+export class ServicesPanelComponent implements OnInit, AfterViewInit {
+  @ViewChild(ServicesTableComponent) servicesTable!: ServicesTableComponent;
+  isGross: boolean = true;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    public snackbarService: SnackbarService,
+    public dialog: MatDialog,
+    public servicesService: ServicesService
+  ) {}
+  ngAfterViewInit(): void {
+    this.servicesTable.dataSource.loadServices();
   }
 
+  ngOnInit(): void {}
+
+  onAddService() {
+    const dialogRef = this.dialog.open(CreateServiceDialogComponent, {
+      width: '600px'
+    });
+
+    dialogRef.afterClosed().subscribe((service: Service) => {
+      if (service !== undefined) this.servicesTable.dataSource.addService(service);
+    });
+  }
 }
