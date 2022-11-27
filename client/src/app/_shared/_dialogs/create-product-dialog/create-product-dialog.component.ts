5fe1d45223e59d504d3ade9bae0ebb61b98be733
wiktord2000 <wiktord2000@wp.pl>
2022-11-27 11:40:22 +0100

    Update CreateProductDialog

--- client/src/app/_shared/_dialogs/create-product-dialog/create-product-dialog.component.ts
+++ client/src/app/_shared/_dialogs/create-product-dialog/create-product-dialog.component.ts
@@ -8,7 +8,7 @@ import { SnackbarService } from 'src/app/_services/snackbar.service';
 
 const FLOAT_REGEX = /^[0-9]*\.[0-9]{2}$/;
 const INTEGER_REGEX = /^\+?(0|[1-9]\d*)$/;
-const ZERO_TO_HUNDRED_REGEX = /\b([0-9]|[1-9][0-9]|100)\b/;
+const ZERO_TO_HUNDRED_REGEX = /\b([0-9]|[1-9][0-9])\b/; // without 100
 
 @Component({
   selector: 'app-create-product-dialog',
@@ -22,14 +22,14 @@ export class CreateProductDialogComponent implements OnInit {
     name: ['', [Validators.required]],
     manufacturer: ['', [Validators.required]],
     unit: ['szt.', [Validators.required]],
-    code: [''],
-    availability: ['0', [Validators.pattern(INTEGER_REGEX)]],
+    code: ['', [Validators.required]],
+    availability: ['0', [Validators.required, Validators.pattern(INTEGER_REGEX)]],
     ean: [''],
-    buyPriceNet: ['0.00', [Validators.pattern(FLOAT_REGEX)]],
-    buyPriceGross: ['0.00', [Validators.pattern(FLOAT_REGEX)]],
-    margin: ['0', [Validators.pattern(ZERO_TO_HUNDRED_REGEX)]],
-    salesPriceNet: ['0.00', [Validators.pattern(FLOAT_REGEX)]],
-    salesPriceGross: ['0.00', [Validators.pattern(FLOAT_REGEX)]]
+    buyPriceNet: ['0.00', [Validators.required, Validators.pattern(FLOAT_REGEX)]],
+    buyPriceGross: ['0.00', [Validators.required, Validators.pattern(FLOAT_REGEX)]],
+    margin: ['25', [Validators.required, Validators.pattern(ZERO_TO_HUNDRED_REGEX)]],
+    salesPriceNet: ['0.00', [Validators.required, Validators.pattern(FLOAT_REGEX)]],
+    salesPriceGross: ['0.00', [Validators.required, Validators.pattern(FLOAT_REGEX)]]
   });
 
   constructor(
@@ -41,30 +41,63 @@ export class CreateProductDialogComponent implements OnInit {
 
   ngOnInit(): void {
     // Subscribe inputs changes
-    // this.productForm.controls['costNet'].valueChanges.subscribe((value: string) => {
-    //   const costGross = Number(value) * 1.23;
-    //   this.productForm.controls['costGross'].setValue(costGross.toFixed(2), { emitEvent: false });
-    //   this.updateTotalPrices();
-    // });
-    // this.productForm.controls['costGross'].valueChanges.subscribe((value: string) => {
-    //   const costNet = Number(value) * 0.77;
-    //   this.productForm.controls['costNet'].setValue(costNet.toFixed(2), { emitEvent: false });
-    //   this.updateTotalPrices();
-    // });
-    // this.productForm.controls['estimatedTime'].valueChanges.subscribe((value: string) => {
-    //   const totalNet = Number(value) * Number(this.productForm.controls['costNet'].value);
-    //   const totalGross = Number(value) * Number(this.productForm.controls['costGross'].value);
-    //   this.productForm.controls['totalNet'].setValue(totalNet.toFixed(2));
-    //   this.productForm.controls['totalGross'].setValue(totalGross.toFixed(2));
-    // });
+    this.productForm.controls['buyPriceNet'].valueChanges.subscribe((value: string) => {
+      this.productForm.controls['buyPriceGross'].setValue((Number(value) * 1.23).toFixed(2), {
+        emitEvent: false
+      });
+      this.calculateDown();
+    });
+
+    this.productForm.controls['buyPriceGross'].valueChanges.subscribe((value: string) => {
+      this.productForm.controls['buyPriceNet'].setValue((Number(value) / 1.23).toFixed(2), {
+        emitEvent: false
+      });
+      this.calculateDown();
+    });
+
+    this.productForm.controls['margin'].valueChanges.subscribe((value: string) => {
+      this.calculateDown();
+    });
+
+    this.productForm.controls['salesPriceNet'].valueChanges.subscribe((value: string) => {
+      this.productForm.controls['salesPriceGross'].setValue((Number(value) * 1.23).toFixed(2), {
+        emitEvent: false
+      });
+      this.calculateUp();
+    });
+
+    this.productForm.controls['salesPriceGross'].valueChanges.subscribe((value: string) => {
+      this.productForm.controls['salesPriceNet'].setValue((Number(value) / 1.23).toFixed(2), {
+        emitEvent: false
+      });
+      this.calculateUp();
+    });
   }
 
-  updateTotalPrices() {
-    const estimatedTime = Number(this.productForm.controls['estimatedTime'].value);
-    const totalNet = estimatedTime * Number(this.productForm.controls['costNet'].value);
-    const totalGross = estimatedTime * Number(this.productForm.controls['costGross'].value);
-    this.productForm.controls['totalNet'].setValue(totalNet.toFixed(2));
-    this.productForm.controls['totalGross'].setValue(totalGross.toFixed(2));
+  // Calculate up fields (buyPriceNet and buyPriceGross)
+  calculateUp() {
+    // buyPrice = salesPrice*(1 - margin)
+    const { salesPriceNet, margin } = this.productForm.value;
+    const buyPriceNet = Number(salesPriceNet) * (1 - Number(margin) / 100);
+    this.productForm.controls['buyPriceNet'].setValue(buyPriceNet.toFixed(2), {
+      emitEvent: false
+    });
+    this.productForm.controls['buyPriceGross'].setValue((buyPriceNet * 1.23).toFixed(2), {
+      emitEvent: false
+    });
+  }
+
+  // Calculate down fields (salesPriceNet and salesPriceGross)
+  calculateDown() {
+    // salesPrice = buyPrice/(1 - margin)
+    const { buyPriceNet, margin } = this.productForm.value;
+    const salesPriceNet = Number(buyPriceNet) / (1 - Number(margin) / 100);
+    this.productForm.controls['salesPriceNet'].setValue(salesPriceNet.toFixed(2), {
+      emitEvent: false
+    });
+    this.productForm.controls['salesPriceGross'].setValue((salesPriceNet * 1.23).toFixed(2), {
+      emitEvent: false
+    });
   }
 
   onSaveChanges() {
@@ -72,9 +105,8 @@ export class CreateProductDialogComponent implements OnInit {
 
     let formValue = this.productForm.value;
 
-    // Create case
     this.productsService
-      .addProduct(formValue)
+      .addProduct({ ...formValue, margin: formValue.margin / 100 })
       .pipe(
         finalize(() => {
           this.isSaving = false;
