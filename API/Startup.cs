3eff255ab6483f77eb12090b6cd5265d5c976edc
wiktord2000 <wiktord2000@wp.pl>
2022-11-28 20:32:53 +0100

    Enable Sawgger

--- API/Startup.cs
+++ API/Startup.cs
@@ -39,13 +39,12 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {   
-            // Delete when create middleware
-            // if (env.IsDevelopment())
-            // {
-            //     app.UseDeveloperExceptionPage();
-            //     app.UseSwagger();
-            //     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
-            // }
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
+            }
             app.UseMiddleware<ExceptionMiddleware>();
 
             app.UseHttpsRedirection();
