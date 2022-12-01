9b9318e820eec4599c479e968a96accbe8030876
wiktord2000 <wiktord2000@wp.pl>
2022-12-01 22:04:33 +0100

    Bind CreateOrderDialog to OrdersPanel

--- client/src/app/panels/orders-panel/orders-panel.component.ts
+++ client/src/app/panels/orders-panel/orders-panel.component.ts
@@ -5,8 +5,9 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { SnackbarService } from 'src/app/_services/snackbar.service';
 import { FormControl } from '@angular/forms';
 import { OrdersTableComponent } from 'src/app/_shared/_tables/orders-table/orders-table.component';
-
-
+import { MatDialog } from '@angular/material/dialog';
+import { CreateOrderDialogComponent } from 'src/app/_shared/_dialogs/create-order-dialog/create-order-dialog.component';
+import { Order } from 'src/app/_models/Order';
 
 @Component({
   selector: 'app-orders-panel',
@@ -14,24 +15,32 @@ import { OrdersTableComponent } from 'src/app/_shared/_tables/orders-table/order
   styleUrls: ['./orders-panel.component.css']
 })
 export class OrdersPanelComponent implements OnInit, AfterViewInit {
-
   @ViewChild(OrdersTableComponent) ordersTable!: OrdersTableComponent;
-  statusControl = new FormControl(['0','1','2','3']);
-  
-  constructor(public snackbarService : SnackbarService, 
-              public ordersService: OrdersService) {
-  }
+  statusControl = new FormControl(['0', '1', '2', '3']);
+
+  constructor(
+    public snackbarService: SnackbarService,
+    public ordersService: OrdersService,
+    public dialog: MatDialog
+  ) {}
   ngAfterViewInit(): void {
     this.ordersTable.dataSource.loadOrders(this.statusControl.value.map(Number));
   }
 
   ngOnInit(): void {}
 
-  onToggleGroupChange(){
-    (!this.statusControl.value.length) 
+  onToggleGroupChange() {
+    !this.statusControl.value.length
       ? this.ordersTable.dataSource.setOrders([])
       : this.ordersTable.dataSource.loadOrders(this.statusControl.value.map(Number));
   }
-  
-}
 
+  onAddOrder() {
+    const dialogRef = this.dialog.open(CreateOrderDialogComponent, {
+      width: '800px'
+    });
+    dialogRef.afterClosed().subscribe((order: Order) => {
+      if (order !== undefined) this.ordersTable.dataSource.addOrder(order);
+    });
+  }
+}
