302d99723bf02157bebd315f85ece0363b554c8a
wiktord2000 <wiktord2000@wp.pl>
2022-12-01 22:03:33 +0100

    Update CustomHeaderComponent

--- client/src/app/_shared/custom-header/custom-header.component.ts
+++ client/src/app/_shared/custom-header/custom-header.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CustomHeaderComponent implements OnInit {
   @Input() headerText: string;
+  @Input() disableTopDivider: boolean = false;
 
   constructor() {}
 
