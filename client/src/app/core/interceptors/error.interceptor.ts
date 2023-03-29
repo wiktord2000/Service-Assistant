84f398dc3b1e16314fec903e3dcbebed91a3cfa1
wiktord2000 <wiktord2000@wp.pl>
2023-03-29 21:57:15 +0200

    Refactor ErrorInterceptor

--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -11,43 +11,56 @@ export class ErrorInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
       catchError((error) => {
-        if (error) {
-          switch (error.status) {
-            case 400: // we have to handle to types of 400 response (array of errors and single error (string))
-              if (error.error.errors) {
-                let modalStateErrors = [];
-                for (const key in error.error.errors) {
-                  if (error.error.errors[key]) {
-                    modalStateErrors.push(error.error.errors[key]);
-                  }
-                }
-                modalStateErrors = modalStateErrors.flat();
-                if (modalStateErrors.length) throw modalStateErrors; // instead of snackbar we will print them at page (many validation errors)
-              } else {
-                this.snackbarService.showMessage('error', error.error, undefined, false);
+        // Handle specific errors
+        switch (error?.status) {
+          // Bad Request
+          case 400:
+            if (!error?.error?.errors) {
+              // Single error message
+              this.snackbarService.showMessage('error', error?.error, undefined, false);
+              break;
+            }
+            // Iterate on object and collect the errors arrays
+            let errorsArrays: Array<string[]> = [];
+            for (const key in error.error.errors) {
+              if (error.error.errors[key]) {
+                errorsArrays.push(error.error.errors[key]);
               }
-              break;
-            case 401:
-              this.snackbarService.showMessage('error', error.error, undefined, false);
-              break;
-            case 404:
-              this.router.navigateByUrl('/not-found');
-              break;
-            case 500:
-              const navigationExtras: NavigationExtras = { state: { error: error.error } }; //attach data to navigation route
-              this.router.navigateByUrl('/server-error', navigationExtras);
-              break;
-            default:
-              this.snackbarService.showMessage(
-                'error',
-                `Nieznany błąd. Spróbuj ponownie...`,
-                undefined,
-                false
-              );
-              console.log(error);
-              break;
-          }
+            }
+            let errorsArray = errorsArrays.flat();
+            // Multiple errors message
+            if (errorsArray.length)
+              this.snackbarService.showMessage('error', errorsArray.join(' | '), undefined, false);
+            break;
+
+          // Unauthorized
+          case 401:
+            this.snackbarService.showMessage('error', error?.error, undefined, false);
+            break;
+
+          // Not found
+          case 404:
+            this.router.navigateByUrl('/not-found');
+            break;
+
+          // Server error
+          case 500:
+            // Attach data to navigation route & redirect
+            const navigationExtras: NavigationExtras = { state: { error: error?.error } };
+            this.router.navigateByUrl('/server-error', navigationExtras);
+            break;
+
+          default:
+            this.snackbarService.showMessage(
+              'error',
+              `Unknown error. Try again later...`,
+              undefined,
+              false
+            );
+            console.log(error);
+            break;
         }
+        // Propagate the error
         return throwError(() => error);
       })
     );
