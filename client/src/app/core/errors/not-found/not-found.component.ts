a8e14555d56b9e113e10aee4b288d0a689544db6
wiktord2000 <wiktord2000@wp.pl>
2023-04-05 18:29:41 +0200

    NotFoundComponent refactor

--- client/src/app/core/errors/not-found/not-found.component.ts
+++ client/src/app/core/errors/not-found/not-found.component.ts
@@ -6,7 +6,7 @@ import { firstValueFrom } from 'rxjs';
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
+  styleUrls: ['./not-found.component.scss']
 })
 export class NotFoundComponent implements OnInit {
   constructor(private router: Router, private accountService: AccountService) {}
