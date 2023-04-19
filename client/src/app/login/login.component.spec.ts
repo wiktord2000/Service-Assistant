a5c02dc8b298078f5cbc05b302191bdacdb83aa5
wiktord2000 <wiktord2000@wp.pl>
2023-04-19 21:11:46 +0200

    Test LoginComponent

--- client/src/app/login/login.component.spec.ts
+++ client/src/app/login/login.component.spec.ts
@@ -1,6 +1,11 @@
-// import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
+import { SnackbarService } from '../core/services/ui/snackbar.service';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { AccountService } from '../core/services/http/account.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormBuilder } from '@angular/forms';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -9,21 +14,27 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule, MatSnackBarModule, HttpClientTestingModule],
       declarations: [LoginComponent],
-      // schemas: [CUSTOM_ELEMENTS_SCHEMA],
-      // providers: [{ provide: MyService, useValue: {} }],
-      imports: []
+      // schemas: [NO_ERRORS_SCHEMA],
+      providers: [SnackbarService, AccountService, FormBuilder]
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
     // myService = TestBed.inject(MyService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display "Logowanie" title', () => {
+    fixture.detectChanges();
+    let complied: HTMLElement = fixture.nativeElement;
+    expect(complied.querySelector('h1').textContent).toEqual('Logowanie');
   });
 });
