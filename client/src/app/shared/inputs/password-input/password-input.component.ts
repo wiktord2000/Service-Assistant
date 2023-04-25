9d36a60c065aecbecb9ffd68d90916fda62aae8b
wiktord2000 <wiktord2000@wp.pl>
2023-04-25 22:41:12 +0200

    PasswordInputComponent update

--- client/src/app/shared/inputs/password-input/password-input.component.ts
+++ client/src/app/shared/inputs/password-input/password-input.component.ts
@@ -4,25 +4,23 @@ import { NgControl, ControlValueAccessor } from '@angular/forms';
 @Component({
   selector: 'app-password-input',
   templateUrl: './password-input.component.html',
-  styleUrls: ['./password-input.component.css']
+  styleUrls: ['./password-input.component.scss']
 })
 export class PasswordInputComponent implements ControlValueAccessor {
-
   @Input() label: string;
   @Input() isTrigger: boolean = true;
   isPasswordVisible: boolean = false;
-  
-  constructor(@Self() public ngControl: NgControl) { 
-    this.ngControl.valueAccessor = this; 
+
+  constructor(@Self() public ngControl: NgControl) {
+    this.ngControl.valueAccessor = this;
   }
 
   togglePasswordVisibility(): void {
     this.isPasswordVisible = !this.isPasswordVisible;
   }
-  
+
   // We don't have to use it
   writeValue(obj: any): void {}
   registerOnChange(fn: any): void {}
   registerOnTouched(fn: any): void {}
-
 }
