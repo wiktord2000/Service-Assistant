7fbc1285298ea69bdbb617033d74a7acecf0637a
wiktord2000 <wiktord2000@wp.pl>
2023-04-26 22:45:29 +0200

    Fix routing problem (temporary solution)

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { AccountService } from './core/services/http/account.service';
 import { Component, OnInit } from '@angular/core';
 import { User } from './core/models/User';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,15 +9,15 @@ import { User } from './core/models/User';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor(private accountService: AccountService) {}
-
+  constructor(private accountService: AccountService, private router: Router) {}
   ngOnInit(): void {
+    // Check whether user is stored inside local storage
     this.setCurrentUser();
   }
 
-  // When app starts check stored user in localstorage
   setCurrentUser() {
     const user: User = JSON.parse(localStorage.getItem('user'));
+    if (user) this.router.navigate(['/orders']);
     this.accountService.setCurrentUser(user);
   }
 }
