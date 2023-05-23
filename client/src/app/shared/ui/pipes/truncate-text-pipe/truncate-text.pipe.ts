da60757482e4e935aa2c3b4fd0541cbea8882811
wiktord2000 <wiktord2000@wp.pl>
2023-05-23 10:53:02 +0200

    Update TruncateText pipe

--- client/src/app/shared/ui/pipes/truncate-text-pipe/truncate-text.pipe.ts
+++ client/src/app/shared/ui/pipes/truncate-text-pipe/truncate-text.pipe.ts
@@ -5,6 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TruncateTextPipe implements PipeTransform {
   transform(text: string, limit: number): string {
+    if (!text) return '';
     return text.length > limit ? text.substring(0, limit) + '...' : text;
   }
 }
