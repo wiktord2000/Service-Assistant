5250f6c1f56627801cd8610a0e6cfe6910250e4b
wiktord2000 <wiktord2000@wp.pl>
2023-07-12 23:06:38 +0200

    Initial implementation of BasicLink

--- client/src/app/shared/ui/links/basic-link/basic-link.component.ts
+++ client/src/app/shared/ui/links/basic-link/basic-link.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+import { TooltipPosition } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-basic-link',
@@ -6,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./basic-link.component.scss']
 })
 export class BasicLinkComponent implements OnInit {
+  @Input() routerLink!: string;
+  @Input() displayText!: string;
+  @Input() tooltipText?: string;
+  @Input() tooltipPosition: TooltipPosition = 'right';
+  @Input() paletteColor: ThemePalette = 'primary';
+  @Input() customColor?: string;
+  @Input() icon?: string;
+  @Input() label?: string;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.routerLink === undefined || this.displayText === undefined) {
+      throw new Error(
+        'Properties "routerLink" and "displayText" are required for proper rendering of the component.'
+      );
+    }
+  }
 }
--- client/src/app/shared/ui/links/basic-link/basic-link.module.ts
+++ client/src/app/shared/ui/links/basic-link/basic-link.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BasicLinkComponent } from './basic-link.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [BasicLinkComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatTooltipModule, MatIconModule, RouterModule],
   exports: [BasicLinkComponent]
 })
 export class BasicLinkModule {}
