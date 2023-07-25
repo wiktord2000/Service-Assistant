ed5fc449ac58cc1b4ff97ae2f73fc60096c29ca0
wiktord2000 <wiktord2000@wp.pl>
2023-07-25 22:23:42 +0200

    Update VehicleFormModule

--- client/src/app/features/vehicles/ui/vehicle-form/vehicle-form.module.ts
+++ client/src/app/features/vehicles/ui/vehicle-form/vehicle-form.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VehicleFormComponent } from './vehicle-form.component';
+import { TextInputModule } from 'src/app/shared/ui/inputs/text-input/text-input.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DateInputModule } from 'src/app/shared/ui/inputs/date-input/date-input.module';
+import { ClientSelectInputModule } from 'src/app/shared/ui/selectors/client-select-input/client-select-input.module';
 
 @NgModule({
   declarations: [VehicleFormComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    TextInputModule,
+    ClientSelectInputModule,
+    DateInputModule,
+    ReactiveFormsModule
+  ],
   exports: [VehicleFormComponent]
 })
 export class VehicleFormModule {}
