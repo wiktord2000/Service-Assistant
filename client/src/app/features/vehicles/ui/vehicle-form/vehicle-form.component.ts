a2e35a8bc201f6583782acc816d4ead8f5751316
wiktord2000 <wiktord2000@wp.pl>
2023-07-25 22:23:26 +0200

    Update of VehicleFormComponent

--- client/src/app/features/vehicles/ui/vehicle-form/vehicle-form.component.ts
+++ client/src/app/features/vehicles/ui/vehicle-form/vehicle-form.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { VehicleFormGroup } from './vehicle-form-types';
+import { Client } from 'src/app/core/models/Client';
+
+const DEFAULT_VEHICLE_FORM_ID = 'vehicle-form'; // Purpose: attaching external buttons
 
 @Component({
   selector: 'app-vehicle-form',
@@ -6,7 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehicle-form.component.scss']
 })
 export class VehicleFormComponent implements OnInit {
+  @Input() formId: string = DEFAULT_VEHICLE_FORM_ID;
+  @Input() vehicleForm!: VehicleFormGroup;
+  @Input() vehicleOwner!: Client;
+  @Output() onSubmit: EventEmitter<SubmitEvent> = new EventEmitter();
+  @Output() onVehicleOwnerChange: EventEmitter<Client> = new EventEmitter();
+  currentDate: Date;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.currentDate = new Date();
+  }
+
+  onSubmitForm(event: SubmitEvent) {
+    this.onSubmit.emit(event);
+  }
+
+  onClientChange(client: Client) {
+    this.onVehicleOwnerChange.emit(client);
+  }
 }
