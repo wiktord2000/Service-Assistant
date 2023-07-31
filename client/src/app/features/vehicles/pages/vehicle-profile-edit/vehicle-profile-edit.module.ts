450d7c9c897ce92750d29f0c2e481db5cb7edb4c
wiktord2000 <wiktord2000@wp.pl>
2023-07-31 23:36:07 +0200

    Update VehicleProfileEditModule

--- client/src/app/features/vehicles/pages/vehicle-profile-edit/vehicle-profile-edit.module.ts
+++ client/src/app/features/vehicles/pages/vehicle-profile-edit/vehicle-profile-edit.module.ts
@@ -3,22 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { VehicleProfileEditRoutingModule } from './vehicle-profile-edit-routing.module';
 import { VehicleProfileEditComponent } from './vehicle-profile-edit.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { DateInputModule } from 'src/app/shared/ui/inputs/date-input/date-input.module';
-import { TextInputModule } from 'src/app/shared/ui/inputs/text-input/text-input.module';
-import { ClientSelectInputModule } from 'src/app/shared/ui/selectors/client-select-input/client-select-input.module';
+import { VehicleFormModule } from '../../ui/vehicle-form/vehicle-form.module';
 
 @NgModule({
   declarations: [VehicleProfileEditComponent],
-  imports: [
-    CommonModule,
-    VehicleProfileEditRoutingModule,
-    MatButtonModule,
-    TextInputModule,
-    ClientSelectInputModule,
-    DateInputModule,
-    ReactiveFormsModule
-  ]
+  imports: [CommonModule, VehicleProfileEditRoutingModule, MatButtonModule, VehicleFormModule]
 })
 export class VehicleProfileEditModule {}
