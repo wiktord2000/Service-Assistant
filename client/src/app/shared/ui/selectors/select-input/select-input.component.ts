263215f365c6f899328cbbf9b1fb14f2ff9dbd9b
wiktord2000 <wiktord2000@wp.pl>
2023-08-20 22:36:33 +0200

    Update SelectInputComponent

--- client/src/app/shared/ui/selectors/select-input/select-input.component.ts
+++ client/src/app/shared/ui/selectors/select-input/select-input.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
-import { AbstractControl } from '@angular/forms';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, Self } from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { Subscription, debounceTime, distinctUntilChanged } from 'rxjs';
 
@@ -8,26 +8,27 @@ import { Subscription, debounceTime, distinctUntilChanged } from 'rxjs';
   templateUrl: './select-input.component.html',
   styleUrls: ['./select-input.component.scss']
 })
-export class SelectInputComponent implements OnInit, OnDestroy {
+export class SelectInputComponent implements OnInit, OnDestroy, ControlValueAccessor {
   @Input() label?: string;
   @Input() optionIcon?: string;
   @Input() selectedValue?: Object;
   @Input() possibleValues?: Object[];
   @Input() displayValueAs: (value: Object) => string;
-  @Input() formControl!: AbstractControl;
   @Output() onAddClick: EventEmitter<MouseEvent> = new EventEmitter();
   @Output() onOptionSelected: EventEmitter<MatAutocompleteSelectedEvent> = new EventEmitter();
   @Output() onInput: EventEmitter<string> = new EventEmitter();
   valueChangesSubsctiption: Subscription;
 
-  constructor() {}
+  constructor(@Self() public ngControl: NgControl) {
+    this.ngControl.valueAccessor = this;
+  }
 
   ngOnInit(): void {
     this.subscribeValueChanges();
   }
 
   subscribeValueChanges() {
-    this.valueChangesSubsctiption = this.formControl.valueChanges
+    this.valueChangesSubsctiption = this.ngControl.control.valueChanges
       .pipe(debounceTime(300), distinctUntilChanged())
       .subscribe((value: string | Object) => {
         // When we will get object - do nothing (mat-autocomplate defect)
@@ -40,21 +41,26 @@ export class SelectInputComponent implements OnInit, OnDestroy {
   updateSelectedValue(value?: Object) {
     this.selectedValue = value;
     this.possibleValues = [value];
-    this.formControl.setValue(this.displayValueAs(value), { emitEvent: false });
+    this.ngControl.control.setValue(this.displayValueAs(value), { emitEvent: false });
   }
 
   reset(inputValue: string, emitInputEvent: boolean = false) {
     this.selectedValue = null;
     this.possibleValues = [];
-    this.formControl.setValue(inputValue, { emitEvent: emitInputEvent });
+    this.ngControl.control.setValue(inputValue, { emitEvent: emitInputEvent });
   }
 
   setError(type: 'notExist' | 'required') {
     const error = type === 'required' ? { required: true } : { 'not-exist': true };
-    this.formControl.setErrors(error);
+    this.ngControl.control.setErrors(error);
   }
 
   ngOnDestroy(): void {
     this.valueChangesSubsctiption.unsubscribe();
   }
+
+  // We don't have to use it
+  writeValue(obj: any): void {}
+  registerOnChange(fn: any): void {}
+  registerOnTouched(fn: any): void {}
 }
